45d8a32099caad9f9d929acd828786c935ef0e86
EuanR-D <erenfreydouglas@gmail.com>
2025-10-01 10:36:37 +1000

    Q4.1 Create movie class

---
+++ Part 4/scripts.js
@@ -0,0 +1,13 @@
+// Q4.4
+// creating a movie class with a constructor that gives 4 properties about the movie
+class Movie {
+  constructor(movieID, title, year, rating) {
+    this.movieID = movieID;
+    this.title = title;
+    this.year = year;
+    this.rating = rating;
+  }
+}
+
+console.log(Movie);
+
