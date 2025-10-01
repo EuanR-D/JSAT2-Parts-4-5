d4fde58027cb961cbbb08af8745a3fbac32ec2bc
EuanR-D <erenfreydouglas@gmail.com>
2025-10-01 13:49:36 +1000

    Q4.2 10 mockup movies array

--- Part 4/scripts.js
+++ Part 4/scripts.js
@@ -1,4 +1,4 @@
-// Q4.4
+// Q4.1
 // creating a movie class with a constructor that gives 4 properties about the movie
 class Movie {
   constructor(movieID, title, year, rating) {
@@ -10,4 +10,23 @@ class Movie {
 }
 
 console.log(Movie);
+
+
+// Q4.2
+// Constructing 10 mockup movies with all propeties
+let bigPineapple = new Movie (3, "Big Pineapple", 1656, 3.2);
+let someWarThing = new Movie (7, "Some War Thing", 2027, 0.0);
+let herlockSholmes = new Movie (1, "Herlock Sholmes", 1998, 8.7);
+let lordOfTheFries = new Movie (2, "Lord of the Fries", 2002, 9.6);
+let treeGrump = new Movie (9, "Tree Grump", 1977, 7.0);
+let barkNight = new Movie (4, "Bark Night", 2011, 9.0);
+let georgeLupus = new Movie (6, "George Lupus", 1990, 7.9);
+let fairyGodfather = new Movie (8, "Fairy Godfather", 1989, 8.6);
+let badFellas = new Movie (5, "Bad Fellas", 1991, 9.9);
+let space1002 = new Movie (10, "1002 Space", 1002, 8.5);
+
+// putting all 10 movies into an array, with ID unordered
+let tenMovieList = [bigPineapple, someWarThing, herlockSholmes, lordOfTheFries, treeGrump, barkNight, georgeLupus, fairyGodfather, badFellas, space1002];
+
+console.log(tenMovieList);
 
